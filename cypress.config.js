638309bf9114ceffd7d33f4b40a5c7208d23f380
dharma <dharma@telio.vn>
2023-08-14 15:45:38 +0530

    add reports and videos directory

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,13 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/results',
+    overwrite: true,
+    html: true,
+    json: true,
+  },
 
   e2e: {
     setupNodeEvents(on, config) {
